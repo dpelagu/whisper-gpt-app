210cc803f4c26a39681640a44a853d98cacf6590
dpelagu <132692067+dpelagu@users.noreply.github.com>
2023-05-05 16:20:24 +0000

    first app version

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title("Audio Player")
+# Create file upload component
+uploaded_file = st.file_uploader("Choose an audio file", type=["mp3", "wav"])
+
+# Check if a file is uploaded
+if uploaded_file is not None:
+
+    # Read file contents
+    file_content = uploaded_file.read()
+    
